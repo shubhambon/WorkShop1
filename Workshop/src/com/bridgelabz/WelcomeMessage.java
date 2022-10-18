e4f2f8110b65a8d07284480d1a94d88439512670
shubham <shubham.bondre.1995@gmail.com>
2022-10-18 08:47:32 +0530

    Creating A Welcome Message Programe

---
+++ Workshop/src/com/bridgelabz/WelcomeMessage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Hello Everyone");
+        System.out.println("Welcome To The Java");
+    }
+}
